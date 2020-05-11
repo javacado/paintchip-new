48d50127bf2db6e69dc540f62439e86db78209a0
John Tyner <john@revrocket.us>
2020-05-11 07:09:57 -0700

    commit for push

--- helper/application/controllers/Extractor.php
+++ helper/application/controllers/Extractor.php
@@ -784,7 +784,7 @@ class Extractor extends CI_Controller {
 				if (strpos(strtolower($title), strtolower($tt)) === FALSE) {
 					$title = $tt . " " . $title;
 				}
-				$up = array("title" => $title, "ttlchecked" => 1);
+				$up = array("title" => $title, "ttlchecked" => 1, "approved" => 0);
 				if (($rr->price != '' && $rr->price != '0') || $rr->approved == 1) {
 					$up['approved'] = 1;
 				}
@@ -837,7 +837,7 @@ class Extractor extends CI_Controller {
 			if (($rr->price != '' && $rr->price != '0') || $rr->approved == 1) {
 				$up['approved'] = 1;
 			}
-			echo "<P>new title: " . $title . "(" . $rr->title . ") - $" . $rr->price . " - A:" . $up['approved'];
+			echo "<P>new title: " . $title . "(" . $rr->title . ") - $" . $rr->price . " - C:" . $rr->category . " - A:" . $up['approved'];
 			if ($go) {
 				$this->db->update("jt_supplier_data", $up, array("id" => $rr->id));
 			}
