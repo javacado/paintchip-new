1edb70cfd8ff5c3ab8db9dba82b69ab4f8a72128
John Tyner <john@revrocket.us>
2020-07-01 16:18:44 -0700

    up

--- helper/application/controllers/Extractor.php
+++ helper/application/controllers/Extractor.php
@@ -3592,13 +3592,6 @@ post_mime_type like image/jpeg
 
 	function loopimages() {
 
-		$arrContextOptions = array(
-			"ssl" => array(
-				"verify_peer" => false,
-				"verify_peer_name" => false,
-			),
-		);
-
 		$items = $this->getIarr();
 
 		foreach ($items as $item) {
@@ -3606,7 +3599,26 @@ post_mime_type like image/jpeg
 				continue;
 			}
 
-			$html = file_get_html("https://www.upcitemdb.com/upc/" . $item['upc'], false, stream_context_create($arrContextOptions));
+			$url = "https://www.upcitemdb.com/upc/" . $item['upc']; //, false, stream_context_create($arrContextOptions));
+
+			$options = array(
+				CURLOPT_RETURNTRANSFER => true, // return web page
+				CURLOPT_SSL_VERIFYHOST => 0, // do not return headers
+				CURLOPT_SSL_VERIFYPEER => 0, // do not return headers
+				CURLOPT_HEADER => false, // do not return headers
+				CURLOPT_FOLLOWLOCATION => true, // follow redirects
+				CURLOPT_USERAGENT => "spider", // who am i
+				CURLOPT_AUTOREFERER => true, // set referer on redirect
+				CURLOPT_CONNECTTIMEOUT => 120, // timeout on connect
+				CURLOPT_TIMEOUT => 120, // timeout on response
+				CURLOPT_MAXREDIRS => 10, // stop after 10 redirects
+			);
+			$ch = curl_init($url);
+			curl_setopt_array($ch, $options);
+			$content = curl_exec($ch);
+			curl_close($ch);
+
+			$html = file_get_html($content);
 
 			$d = $html->find('img.product');
 			if (count($d) == 0) {
