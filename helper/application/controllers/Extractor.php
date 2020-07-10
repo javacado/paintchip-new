a157f59a5369c6371f5e66dac0490460a8d3fcaa
John Tyner <john@revrocket.us>
2020-07-10 13:30:23 -0700

    d

--- helper/application/controllers/Extractor.php
+++ helper/application/controllers/Extractor.php
@@ -73,6 +73,7 @@ class Extractor extends CI_Controller {
 		$rr = $r->result();
 		$r->free_result();
 		$data = array("emails" => $rr);
+		die("<h3>Output</h3><pre>" . print_r($data, 1) . "</pre>");
 		$this->load->view('emails-viewer', $data);
 	}
 
