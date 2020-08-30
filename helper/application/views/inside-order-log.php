e1896044e20c24d5c4dd5c8e19a80b23f37c6475
John Tyner <john@revrocket.us>
2020-08-30 13:42:46 -0700

    new

--- helper/application/views/inside-order-log.php
+++ helper/application/views/inside-order-log.php
@@ -1,47 +1,47 @@
- <div id='prodstatuslist' data-logid="<?=$row->log_id?>">
+ <div id='prodstatuslist' data-logid="<?php =$row->log_id?>">
 <table class='table text-center ptable ' style='width:100%;' >
     <tr class='hdr' ><td style='text-align:left;padding-left:0'>Product</td><td class='text-center' style='width:40px'>Got</td><td class='text-center' style='width:40px;color:#c00;' >Order</td>
-    <?foreach ($row->products as $p) {
+    <?php foreach ($row->products as $p) {
 	$is = 0;
 	if (isset($row->prod_data[$p->product_id])) {
 		$is = $row->prod_data[$p->product_id]['in_store'];
 	}
 	?>
-<tr rel='<?=$p->product_id?>' class='prods'><td style='text-align:left;padding-left:0'>
-    <span style="<?if ($is != 1) {echo "color:#c00;";}?>"><?=$p->order_item_name?></span></td>
+<tr rel='<?php =$p->product_id?>' class='prods'><td style='text-align:left;padding-left:0'>
+    <span style="<?php if ($is != 1) {echo "color:#c00;";}?>"><?php =$p->order_item_name?></span></td>
     <td class='text-center' >
-            <input type='radio' class='prod-radio' style=''  rel='<?=$p->product_id?>' onclick="" name='in_stock_<?=$p->product_id?>' <?if ($is) {echo "checked";}?> value='1' name="in_store" />
+            <input type='radio' class='prod-radio' style=''  rel='<?php =$p->product_id?>' onclick="" name='in_stock_<?php =$p->product_id?>' <?php if ($is) {echo "checked";}?> value='1' name="in_store" />
         </td>
     <td class='text-center' >
-            <input type='radio' class='prod-radio'  style='' rel='<?=$p->product_id?>' onclick="" <?if (!$is) {echo "checked";}?> name='in_stock_<?=$p->product_id?>' value='0' name="need_to_order" />
+            <input type='radio' class='prod-radio'  style='' rel='<?php =$p->product_id?>' onclick="" <?php if (!$is) {echo "checked";}?> name='in_stock_<?php =$p->product_id?>' value='0' name="need_to_order" />
 
     </td>
 </tr>
 
-        <?}?>
+        <?php }?>
 </table>
 <div class='srow'>
     <strong>Employee</strong>
 
-        <input class='form-control' rel='employee' type='text' value="<?=$row->employee?>">
+        <input class='form-control' rel='employee' type='text' value="<?php =$row->employee?>">
 
 </div>
 <div class='srow'>
     <strong> Picked Up</strong>
 
         <div class='form-group'>
-              <input type='checkbox' rel='fdc' style='float:left; margin:5px 10px 0 0;' <?if ($row->picked_up != "") {
+              <input type='checkbox' rel='fdc' style='float:left; margin:5px 10px 0 0;' <?php if ($row->picked_up != "") {
 	echo 'checked';
 }
 ?>  onclick='std(this)' />
- <input class='form-control' rel='picked_up' style='width:75%;' readonly type='date' value="<?=$row->picked_up?>" />
+ <input class='form-control' rel='picked_up' style='width:75%;' readonly type='date' value="<?php =$row->picked_up?>" />
  </div>
 </div>
 
 <div class='srow'>
     <strong> Notes</strong>
 
-<textarea rel='notes' class='form-control'><?=$row->notes?></textarea>
+<textarea rel='notes' class='form-control'><?php =$row->notes?></textarea>
 
 
 </div>
