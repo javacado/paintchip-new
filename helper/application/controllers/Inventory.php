58193881bdf6a2576d5e4de88ee55ac7d1d2d300
John Tyner <john@revrocket.us>
2021-02-11 22:24:21 -0800

    d

--- helper/application/controllers/Inventory.php
+++ helper/application/controllers/Inventory.php
@@ -55,6 +55,11 @@ class Inventory extends CI_Controller
 
 
 			$parts = preg_split('/  +/', $row[0]);
+
+			if (strpos($row[0],'VA10105')!==false) {
+				die("<h3>Output</h3><pre>".print_r($parts,1)."</pre>");
+				
+			}
 			//echo ("<h3>".count($parts)."</h3><pre>" . print_r($parts, 1) . "</pre>");
 			$numparts = count($parts);
 			$first = $second = false;
