281d934ddc5fde35cebdeeff56d43f9b1ee84e51
John Tyner <john@revrocket.us>
2021-08-29 15:53:40 -0700

    inv

--- helper/application/controllers/Inventory.php
+++ helper/application/controllers/Inventory.php
@@ -1,6 +1,8 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-
+ini_set('display_errors', 1);
+		ini_set('display_startup_errors', 1);
+		error_reporting(E_ALL);
 class Inventory extends CI_Controller
 {
 	function __construct()
