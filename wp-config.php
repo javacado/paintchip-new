d038a1580c47ba3649f4f323c0a0075b5d9e69a6
javacado <john@laphona.com>
2025-08-30 05:44:13 -0700

    test

---
+++ wp-config.php
@@ -0,0 +1,116 @@
+<?php
+/** Enable W3 Total Cache */
+define('WP_CACHE', true); // Added by W3 Total Cache
+
+define( 'FS_METHOD', 'direct' );
+
+if ($_SERVER['REMOTE_ADDR']!='99.20.67.73') {
+//phpinfo();
+//die('Sorry, The Paint Chip is down for some maintenance. We will be back soon - thanks!');
+}
+@ini_set('upload_max_size' , '10M' );
+define('WP_MEMORY_LIMIT', '1024M');
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'paintchip');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'f@tG1rl!');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY', 'the whole pride is fed');
+define('SECURE_AUTH_KEY', 'the whole pride is fed');
+define('LOGGED_IN_KEY', 'the whole pride is fed');
+define('NONCE_KEY', 'the whole pride is fed');
+define('AUTH_SALT', 'the whole pride is fed');
+define('SECURE_AUTH_SALT', 'the whole pride is fed');
+define('LOGGED_IN_SALT', 'the whole pride is fed');
+define('NONCE_SALT', 'the whole pride is fed');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+//define('WP_DEBUG', true);
+//define('WP_DEBUG_LOG', true);
+//define('WP_DEBUG_DISPLAY', false);
+//define( 'WP_DEBUG_LOG', '/var/www/logs/wp-errors.log' );
+
+if ($_SERVER['REMOTE_ADDR']=='99.20.67.73') {
+define('WP_DEBUG_DISPLAY', true);
+
+}
+
+/* SES STMP plugin */
+define('WPOSES_AWS_ACCESS_KEY_ID', 'AKIATKK5FWDTQEUFSJWL');
+define('WPOSES_AWS_SECRET_ACCESS_KEY', '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');
+
+
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if (!defined('ABSPATH')) {
+	define('ABSPATH', dirname(__FILE__) . '/');
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
+
